8d99684c5081aa4200fcdfa434c706d753325574
Zlogdan <zlogdan@gmail.com>
2018-05-14 18:22:23 +0300

    +

---
+++ server.js
@@ -0,0 +1,59 @@
+/*
+In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
+server, but for some reason omit a client connecting to it.  I added an 
+example at the bottom.
+Save the following server in example.js:
+*/
+
+var net = require('net');
+
+var HOST = '192.168.1.15';
+var PORT = 1337;
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+net.createServer(function(sock) {
+
+    // We have a connection - a socket object is assigned to the connection automatically
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
+    // Add a 'data' event handler to this instance of socket
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to the socket, the client will receive it as data from the server
+        sock.write('You said "' + data + '"');
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
+    });
+
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
+
+
+
+
+
+
+
+
+/*
+var server = net.createServer(function(socket) {
+	socket.write('Echo server\r\n');
+	socket.pipe(socket);
+});
+
+server.listen(1337, '192.168.1.15');
+
+/*
+And connect with a tcp client from the command line using netcat, the *nix 
+utility for reading and writing across tcp/udp network connections.  I've only 
+used it for debugging myself.
+$ netcat 127.0.0.1 1337
+You should see:
+> Echo server
+*/
